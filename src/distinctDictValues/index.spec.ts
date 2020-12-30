932e8e0b84496f78f650f5d46ba5c5959a2b4267
Javier Bullrich <javier.bullrich@innerspace.eu>
2020-12-30 18:46:36 +0100

    Fixed lack of nested tuple

--- src/distinctDictValues/index.spec.ts
+++ src/distinctDictValues/index.spec.ts
@@ -23,14 +23,7 @@ describe('Distinct values pipe function', () => {
     });
 
     it('Should return expected result from own compare fn', () => {
-        const ownCompare = <T>(previous: T | undefined, current: T): T[] => {
-            if (previous && JSON.stringify(previous) != JSON.stringify(current)) {
-                return [previous, current];
-            }
-            return [];
-        };
-
-        const expectedResult = [[{ a: 1 }, { a: 1, b: 2 }], [{ a: 1, b: 2 }, { a: 3, b: 2 }]];
+        const expectedResult = [['a', 1], ['a', 1], ['b', 2], [['a', 3], ['b', 2]]];
         const result = [];
 
         const obs = from([
@@ -39,12 +32,30 @@ describe('Distinct values pipe function', () => {
             { a: 3, b: 2 },
             { a: 3, b: 2 }
         ])
-            .pipe(distinctDictValues(ownCompare))
+            .pipe(distinctDictValues(<Number>(prev, next) => next === prev))
             .subscribe(c => {
                 result.push(c);
             });
 
         obs.unsubscribe();
         Assert.equal(expectedResult, result);
+    });
+
+    it('Should return tuple when two elements differ', () => {
+        const result = [];
+
+        const obs = from([
+            { a: 1 },
+            { a: 2, b: 2 }
+        ])
+            .pipe(distinctDictValues())
+            .subscribe(c => {
+                result.push(c);
+            });
+
+        obs.unsubscribe();
+        Assert.equal(['a', 1], result[0]);
+        // second value should be a tuple of tuples with both a & b
+        Assert.equal([['a', 2], ['b', 2]], result[1]);
     });
 });
--- src/distinctDictValues/index.ts
+++ src/distinctDictValues/index.ts
@@ -2,28 +2,44 @@ import { Observable } from 'rxjs';
 
 // reference: https://netbasal.com/creating-custom-operators-in-rxjs-32f052d69457
 
-const getDiffer = <T>(obj1: T | undefined, obj2: T): T[] => {
+const getDifferingElements = <T>(obj1: T | undefined, obj2: T, compareFn: compareTypeFn): T[] => {
     let newObj: any[] = [];
     // if the first object is null (for the first loop)
     if (!obj1) {
         for (let keys in obj2) {
             newObj.push(keys);
             newObj.push(obj2[keys]);
+            const value: any[] = [keys, obj2[keys]];
+            return value;
         }
+        return newObj;
     } else {
+        const differingValues = [];
+
         // get all the distinct keys inside a tuple
         for (let keys in obj2) {
-            if (obj1[keys] !== obj2[keys]) {
-                newObj.push(keys);
-                newObj.push(obj2[keys]);
+            if (compareFn(obj1[keys], obj2[keys])) {
+                const difference = [keys, obj2[keys]];
+                differingValues.push(difference);
             }
         }
-    }
 
-    return newObj;
+        // if it's only one value, no need for a tuple
+        if (differingValues.length === 1) {
+            return differingValues[0];
+        }
+
+        return differingValues;
+    }
 };
 
-export default function distinctDictValues<T>(compareFn?: (<T>(previous: T | undefined, current: T) => T[])): (source: Observable<T>) => Observable<T[]> {
+const defaultCompareValues = (prev: any, current: any): boolean => {
+    return prev !== current;
+};
+
+type compareTypeFn = <K>(previous: K | undefined, current: K) => boolean;
+
+export default function distinctDictValues<T>(compareFn?: compareTypeFn): (source: Observable<T>) => Observable<T[]> {
     return function <T>(source: Observable<T>): Observable<T[]> {
         return new Observable<T[]>(subscriber => {
             // object from previous round for comparison
@@ -31,12 +47,7 @@ export default function distinctDictValues<T>(compareFn?: (<T>(previous: T | und
             const subscription = source.subscribe({
                 next(val) {
                     // evaluated result using either own function or given one
-                    let values: T[];
-                    if (compareFn) {
-                        values = compareFn<T>(previous, val);
-                    } else {
-                        values = getDiffer(previous, val);
-                    }
+                    const values = getDifferingElements(previous, val, compareFn ?? defaultCompareValues);
                     // set object to be previous iteration
                     previous = val;
                     // only add non empty objects
