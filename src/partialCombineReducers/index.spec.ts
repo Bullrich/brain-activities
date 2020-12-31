1458394f143451e79c55c95121ac4a7762fd628e
Javier Bullrich <javier.bullrich@innerspace.eu>
2020-12-31 05:22:24 +0100

    Added second test with more than one reducer

--- src/partialCombineReducers/index.spec.ts
+++ src/partialCombineReducers/index.spec.ts
@@ -14,7 +14,27 @@ describe('Partial reducer test', () => {
 
         const rootReducer = partialCombineReducers<number>({ n: subReducer });
         const result = rootReducer({ n: 13, s: 'test' }, { type: 'increment' });
-        console.log(result)
         Assert.equal(result, { n: 14, s: 'test' });
     });
+
+    it("Should work with more than one reducer", () => {
+        function addOne(state: number, action: Action): number {
+            if (action.type === 'increment') {
+                return state + 1;
+            }
+            return state;
+        }
+
+        function addExclamation(state: string, action: Action):string{
+            if(action.type === "exclamation"){
+                return state + "!";
+            }
+             return state;
+        }
+
+        // @ts-ignore
+        const rootReducer = partialCombineReducers<number,string>({ n: addOne, s: addExclamation });
+        Assert.equal(rootReducer({ n: 13, s: 'test' }, { type: 'increment' }), { n: 14, s: 'test' });
+        Assert.equal(rootReducer({ n: 13, s: 'test' }, { type: 'exclamation' }), { n: 13, s: 'test!' });
+    })
 });
--- src/partialCombineReducers/index.ts
+++ src/partialCombineReducers/index.ts
@@ -3,7 +3,8 @@ import { Action } from "redux";
 type reducer<S> = ((stateSlice: S, action: Action) => S);
 type nonStrictedObject = { [key: string]: any };
 
-export function partialCombineReducers<A, B = undefined, C = undefined>(reducer: { [key: string]: reducer<A | B | C> }): (payload: nonStrictedObject, action: Action) => nonStrictedObject {
+export function partialCombineReducers<A, B = undefined, C = undefined>(reducer: { [key: string]: reducer<A | B | C> }):
+    (payload: nonStrictedObject, action: Action) => nonStrictedObject {
     return (payload: nonStrictedObject, action: Action): nonStrictedObject => {
         const newPayload = { ...payload };
         for (const key in payload) {
